588fa98f8df46e73271b8b7f50af1aa9b2591602
Junaidwebedia <72661329+Junaidwebedia@users.noreply.github.com>
2020-10-19 11:05:52 +0600

    git initilized and all files added to git

---
+++ moshiur/css/project1/main.js
@@ -0,0 +1,7 @@
+let header = document.querySelector('.header');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', Function (){
+    header.classList.toggle('menu-open');                           
+                               
+    })
